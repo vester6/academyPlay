d1556843dcfd6ea1cb8d0276132ef400f69d7035
Mads Vester <mave@mir-robots.com>
2024-04-05 13:31:03 +0200

    first commit4

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
+    <p>4 teasers</p>
     <TeaserList :teasers="teasersData" />
+    <p>3 teasers</p>
+    <TeaserList :teasers="teasersData2" />
+    <p>2 teasers</p>
+    <TeaserList :teasers="teasersData3" />
   </div>
 </template>
 
@@ -30,6 +35,39 @@ const teasersData = ref([
     headline: "Teaser 3 Headline",
     body: "An example of how using walls that are too thick can cause a localization error. The robot tries to localize itself by placing as many of the red dots as possible inside the black area.",
   },
+  // Add more teasers as needed
+]);
+const teasersData2 = ref([
+  {
+    imageUrl: "/imageTest.png",
+    headline: "Teaser 31 Headline",
+    body: "This is an example of using 1px/5 cm wide walls. Here the robot is correctly localized: The red dots from the scanner data align with the black lines from the map.",
+  },
+  {
+    imageUrl: "/imageTest.png",
+    headline: "Teaser 32 Headline",
+    body: "Here, the thick wall to the right causes the robot to place itself too far to the right. Similarly, the thick wall at the top displaces it too far towards the top of the map.",
+  },
+  {
+    imageUrl: "/imageTest.png",
+    headline: "Teaser 33 Headline",
+    body: "An example of how using walls that are too thick can cause a localization error. The robot tries to localize itself by placing as many of the red dots as possible inside the black area.",
+  },
+
+  // Add more teasers as needed
+]);
+const teasersData3 = ref([
+  {
+    imageUrl: "/imageTest.png",
+    headline: "Teaser 31 Headline",
+    body: "This is an example of using 1px/5 cm wide walls. Here the robot is correctly localized: The red dots from the scanner data align with the black lines from the map.",
+  },
+  {
+    imageUrl: "/imageTest.png",
+    headline: "Teaser 32 Headline",
+    body: "Here, the thick wall to the right causes the robot to place itself too far to the right. Similarly, the thick wall at the top displaces it too far towards the top of the map.",
+  },
+
   // Add more teasers as needed
 ]);
 </script>
