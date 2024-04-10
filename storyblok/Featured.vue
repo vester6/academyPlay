d984e05c8ba16fd8ccd85e39ecd0e04e1ea62c95
Mads Vester <mave@mir-robots.com>
2024-04-10 15:27:58 +0200

    Storyblok2

---
+++ storyblok/Featured.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-editable="blok">
+        <div>
+            <h1> {{ blok.name }} </h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object })
+</script>
